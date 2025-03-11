0fe453e7afa36a40f98327c4d79a0925f303e39b
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-12 03:20:15 +0530

    Logging messages added

--- ToDoApp/ToDoApp/ToDoApp.Client/Services/ToDoService.cs
+++ ToDoApp/ToDoApp/ToDoApp.Client/Services/ToDoService.cs
@@ -4,16 +4,19 @@ namespace ToDoApp.Client.Services
 {
     public class ToDoService
     {
+        private readonly ILogger<ToDoService> _logger;
         public List<ToDoItem> Tasks { get; set; } = new();
 
         public void AddTask(ToDoItem task)
         {
             Tasks.Add(task);
+            _logger.LogInformation("New task successfully added to the to do list!");
         }
 
         public void DeleteTask(int taskId)
         {
             Tasks.RemoveAll(t => t.Id == taskId);
+            _logger.LogInformation("Task removed from the to do list!");
         }
 
         public void StateToggle(int taskId)
@@ -23,6 +26,7 @@ namespace ToDoApp.Client.Services
             {
                 task.IsDone = !task.IsDone;
             }
+            _logger.LogInformation("Completion state of the task updated!");
         }
     }
 }
