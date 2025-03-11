e9e048b30b72acc0519f9c3d2a14519abdf350cc
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-12 03:03:24 +0530

    ToDoItem model added

---
+++ ToDoApp/ToDoApp/ToDoApp.Client/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Client.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateOnly DueDate { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
