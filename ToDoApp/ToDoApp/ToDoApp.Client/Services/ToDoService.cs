d373e2c430abee691162ffd481c1e6f68d102f35
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-12 03:15:49 +0530

    ToDo service added

---
+++ ToDoApp/ToDoApp/ToDoApp.Client/Services/ToDoService.cs
@@ -0,0 +1,28 @@
+﻿using ToDoApp.Client.Models;
+
+namespace ToDoApp.Client.Services
+{
+    public class ToDoService
+    {
+        public List<ToDoItem> Tasks { get; set; } = new();
+
+        public void AddTask(ToDoItem task)
+        {
+            Tasks.Add(task);
+        }
+
+        public void DeleteTask(int taskId)
+        {
+            Tasks.RemoveAll(t => t.Id == taskId);
+        }
+
+        public void StateToggle(int taskId)
+        {
+            var task = Tasks.FirstOrDefault(t => t.Id == taskId);
+            if (task != null)
+            {
+                task.IsDone = !task.IsDone;
+            }
+        }
+    }
+}
