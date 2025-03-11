62d7d347a9af3b9ef823a33276a9e0aa7eb1960a
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-12 03:26:16 +0530

    Data annotations added to ToDoItem model

--- ToDoApp/ToDoApp/ToDoApp.Client/Models/ToDoItem.cs
+++ ToDoApp/ToDoApp/ToDoApp.Client/Models/ToDoItem.cs
@@ -1,9 +1,15 @@
-﻿namespace ToDoApp.Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Client.Models
 {
     public class ToDoItem
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string.Empty;
         public DateOnly DueDate { get; set; }
         public bool IsDone { get; set; } = false;
