93461872d51905f3963e66f3e1799265b5c1511b
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-16 18:59:16 +0530

    Added Blazored Local Storage

--- ToDoApp/ToDoApp/ToDoApp.Client/Program.cs
+++ ToDoApp/ToDoApp/ToDoApp.Client/Program.cs
@@ -1,7 +1,9 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ToDoApp.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<ToDoService>();
 
 await builder.Build().RunAsync();
