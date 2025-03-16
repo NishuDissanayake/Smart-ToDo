41ad21e933becaf9b5ef3b47e5e0097f06ad1318
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-03-16 17:09:23 +0530

    Added Tailwind CSS

---
+++ ToDoApp/ToDoApp/ToDoApp.Client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./**/*.razor",
+        "./**/*.html",
+        "./**/*.cshtml"
+    ],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
